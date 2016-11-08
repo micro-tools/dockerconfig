8b588bee66f799f495a1d8c2220b736b6814b6da
Elmar Athmer <elmar@athmer.org>
2016-11-08 17:20:33 +0100

    add mocha for tests

--- test/int.test.js
+++ test/int.test.js
@@ -1,7 +1,18 @@
 var dockerconfig = require("./../index.js");
 var conf = require("./testconfig.json");
+var assert = require("assert");
 
-var config = dockerconfig.getConfig(conf);
-config.makeGlobal();
 
-console.log(CONFIG.database);
+describe("dockerconfig", () => {
+
+    before(() => {
+        process.env["NODE_CONFIG_DATABASE_POSTGRES_CONSTRING"] = '[{ \"host\": \"123\", \"port\": 123 }]';
+
+        var config = dockerconfig.getConfig(conf);
+        config.makeGlobal();
+    });
+
+    it("sets the expected values", () =>
+        assert.deepEqual(CONFIG.database, { postgres: { constring: [ {host: "123", port: 123} ] } })
+    );
+});
